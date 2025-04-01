f3342b8060f2edcc7b1c416b0dcc197d7b8a13c7
Marcin Fortuna <marcin.fortuna@onet.eu>
2025-04-01 23:33:29 +0200

    feat(api): bind refactored viewsets to core app

--- backend/core/routers.py
+++ backend/core/routers.py
@@ -1,2 +1,10 @@
 from rest_framework.routers import DefaultRouter
 
+from .viewset_collection.user_view import RegisterViewSet, LoginViewSet
+from .viewset_collection.expenses_view import ExpensesViewSet
+
+router = DefaultRouter()
+
+router.register(r"user", LoginViewSet, basename="login")
+router.register(r"user", RegisterViewSet, basename="register")
+router.register(r"expenses", ExpensesViewSet, basename="expenses")
