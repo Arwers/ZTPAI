233bba7cdcc32fe8ebccd18e9f5af34d316368bb
Marcin Fortuna <marcin.fortuna@onet.eu>
2025-06-01 14:39:01 +0200

    fix(api): fix swagger ui settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -38,6 +38,8 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+
 }
 
 # Configure JWT settings
